f62c2ebbef57c36fbbbc6e03ceb11b3e479a3cc7
vladimirmovsesan <vladimirmovsesan-dev@yandex.ru>
2021-09-23 21:58:27 +0300

    done modify ReverseTranspose

--- Homework/ReverseTranspose/ReverseTranspose.java
+++ Homework/ReverseTranspose/ReverseTranspose.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Reverse {
+public class ReverseTranspose {
 
     public static int[] parseFromString(String t) {
         int countOfDigits = 0, currdigit = 0;
@@ -23,19 +23,34 @@ public class Reverse {
 
     public static void main(String[] args) {
         int[][] reversedAnswer = new int[(int)1e6][];
-        int len = 0;
+        int len = 0, mx = Integer.MIN_VALUE;
         Scanner s = new Scanner(System.in);
 
         while (s.hasNextLine()) {
             reversedAnswer[len++] = parseFromString(s.nextLine());
+            mx = Math.max(mx, reversedAnswer[len - 1].length);
         }
         s.close();
-
-        for (int i = len - 1; i >= 0; i--) {
-            for (int j = reversedAnswer[i].length - 1; j >= 0; j--) {
-                System.out.print(reversedAnswer[i][j] + " ");
+        
+        for (int j = 0; j < mx; j++) {
+            for (int i = 0; i < len; i++) {
+                if (j < reversedAnswer[i].length)
+                    System.out.print(reversedAnswer[i][j] + " ");
             }
             System.out.println();
         }
     }
 }
+/*
+1 2 3
+4 5 6
+7 8 9
+
+1 4 7
+2 5 8
+3 6 9
+
+0 0
+1 0
+0 1
+*/
---
+++ Homework/ReverseTranspose/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+or (int j = 0; j < reversedAnswer[i].length; j++) {
+        //     for (i = 0; i < len; i++) {
+        //         System.out.print(reversedAnswer[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
