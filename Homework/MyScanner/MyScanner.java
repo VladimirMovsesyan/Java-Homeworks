a3b75067def69ee34768b9788da2ec22570735d5
vladimirmovsesan <vladimirmovsesan-dev@yandex.ru>
2021-10-06 12:17:32 +0300

    Formating code to Java Standart

--- Homework/MyScanner/MyScanner.java
+++ Homework/MyScanner/MyScanner.java
@@ -9,7 +9,7 @@ public class MyScanner {
 
     MyScanner(InputStream input) throws IOException {
         this.reader = new BufferedReader(new InputStreamReader(input));
-        this.buff = new char[buffSize];
+        this.buff = new char[this.buffSize];
         this.curPos = 0;
         this.curSize = -2;
         this.sb = new StringBuilder();
@@ -18,7 +18,7 @@ public class MyScanner {
 
     MyScanner(String s) throws IOException {
         this.reader = new BufferedReader(new StringReader(s));
-        this.buff = new char[buffSize];
+        this.buff = new char[this.buffSize];
         this.curPos = 0;
         this.curSize = -2;
         this.sb = new StringBuilder();
@@ -27,7 +27,7 @@ public class MyScanner {
 
     MyScanner(String s, String codec) throws IOException {
         this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(s), codec));
-        this.buff = new char[buffSize];
+        this.buff = new char[this.buffSize];
         this.curPos = 0;
         this.curSize = -2;
         this.sb = new StringBuilder();
@@ -37,7 +37,7 @@ public class MyScanner {
     public boolean read() throws IOException {
         if (this.curPos < this.curSize)
             return true;
-        this.curSize = this.reader.read(buff, 0, buffSize);
+        this.curSize = this.reader.read(this.buff, 0, this.buffSize);
         this.curPos = 0;
         return (this.curSize > -1);
     }
@@ -64,48 +64,48 @@ public class MyScanner {
 
     public boolean hasNextInt() throws IOException {
         skipWhiteSpaces();
-        while ((curPos < curSize || read()) && goodForInt(buff[curPos])) {
-            sb.append(buff[curPos++]);
+        while ((this.curPos < this.curSize || read()) && goodForInt(this.buff[this.curPos])) {
+            this.sb.append(this.buff[this.curPos++]);
         }
-        return (sb.length() > 0);
+        return (this.sb.length() > 0);
     }
 
     public int nextInt() {
-        String temp = sb.toString();
-        sb = new StringBuilder();
+        String temp = this.sb.toString();
+        this.sb = new StringBuilder();
         return Integer.parseInt(temp);
     }
 
     public boolean hasNextLine() throws IOException {
-        while (read() && !(Character.toString(buff[curPos]).equals(System.getProperty("line.separator")))) {
-            sb.append(buff[curPos++]);
+        while (read() && !(Character.toString(this.buff[this.curPos]).equals(System.getProperty("line.separator")))) {
+            this.sb.append(this.buff[this.curPos++]);
             read();
         }
-        curPos += ((Character.toString(buff[curPos]).equals(System.getProperty("line.separator"))) ? 1 : 0);
-        return (curSize != -1);
+        this.curPos += ((Character.toString(this.buff[this.curPos]).equals(System.getProperty("line.separator"))) ? 1 : 0);
+        return (this.curSize != -1);
     }
 
     public String nextLine() {
-        String temp = sb.toString();
-        sb = new StringBuilder();
+        String temp = this.sb.toString();
+        this.sb = new StringBuilder();
         return temp;
     }
 
     public boolean hasNextWord() throws IOException {
         skipNotAllowedSymbols();
-        while ((curPos < curSize || read()) && goodForWord(buff[curPos])) {
-            sb.append(buff[curPos++]);
+        while ((this.curPos < this.curSize || read()) && goodForWord(this.buff[this.curPos])) {
+            this.sb.append(this.buff[this.curPos++]);
         }
-        return (sb.length() > 0);
+        return (this.sb.length() > 0);
     }
 
     public String nextWord() {
-        String temp = sb.toString();
-        sb = new StringBuilder();
+        String temp = this.sb.toString();
+        this.sb = new StringBuilder();
         return temp;
     }
 
     public void close() throws IOException {
-        reader.close();
+        this.reader.close();
     }
 }
